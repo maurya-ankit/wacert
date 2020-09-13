8387a5cd0bdd84f84376d559f56be4f6a40cdefb
maurya-ankit <ankitmaurya18@cse.iiitp.ac.in>
2020-09-13 17:58:49 +0530

    Terms of uses

--- certificate/views.py
+++ certificate/views.py
@@ -4,22 +4,29 @@ import mimetypes
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+count=0;
 
 # Create your views here.
 class home(View):
     def get(self,request):
-        return render(request,'index.html')
+        context={'count':count}
+        return render(request,'index.html',context)
     def post(self,request):
         name=request.POST.get('name')
         img = Image.open("static/cert.png")
         draw = ImageDraw.Draw(img)
         # font = ImageFont.truetype(<font-file>, <font-size>)
         font = ImageFont.truetype("static/Roboto-Regular.ttf", 100)
+        fontd = ImageFont.truetype("static/Roboto-Regular.ttf", 20)
         # draw.text((x, y),"Sample Text",(r,g,b))
         draw.text((740, 550),f"{name}",(0,0,0),font=font)
+        draw.text((650,1370),f"* This is just for fun. Make yours from here : { request.build_absolute_uri() }",(0,0,0),font=fontd)
+
 
         response = HttpResponse(content_type ="image/png")
         img.save(response, "PNG")
+        global count
+        count+=1
         return response
 
         
