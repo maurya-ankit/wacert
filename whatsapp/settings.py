3835ce180bc83796f16bca4b9a7182721d728773
maurya-ankit <ankitmaurya18@cse.iiitp.ac.in>
2020-09-13 17:21:43 +0530

    realpython

--- whatsapp/settings.py
+++ whatsapp/settings.py
@@ -80,8 +80,7 @@ WSGI_APPLICATION = 'whatsapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
     }
 }
 
